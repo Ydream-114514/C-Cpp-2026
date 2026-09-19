4fd00efcd07216083a8932003a4d71c9cab96119
Ydream-114514 <956311770@qq.com>
2026-09-19 14:32:53 +0800

    Add files via upload

---
+++ level1/p02_is_prime/1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(int n){
+    for(int i=2;i*i<=n;++i)
+        if(n%i==0)
+            return 0;
+    return 1;
+}
+int main(){
+    while(1){
+        int n;
+        printf(">>>");
+        scanf("%d",&n);
+        if(check(n)) printf("Number %d is prime\n",n);
+        else printf("Number %d is not prime\n",n);
+    }
+    
+    return 0;
+}
