61d1480b95ea3cda077c8ef9dadf95c890502908
Ydream-114514 <956311770@qq.com>
2026-09-19 16:19:57 +0800

    main.c

--- level1/p04_hanoi/main.c
+++ level1/p04_hanoi/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world!\n");
+#include<stdio.h>
+#include<windows.h>
+void dfs(int n,char a,char b,char c){
+    if(n==1){
+        printf("%c -> %c\n",a,c);
+        return;
+    }
+    dfs(n-1,a,c,b);
+    printf("%c -> %c\n",a,c);
+    dfs(n-1,b,a,c);
+}
+int main(){
+    dfs(3,'A','B','C');
     return 0;
 }
