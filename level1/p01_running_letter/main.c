5fcdfac2f6721cfe34a6714a19d20bd868dd72ec
Ydream-114514 <956311770@qq.com>
2026-09-23 19:17:33 +0800

    Update running letter program

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,18 +1,43 @@
 #include<stdio.h>
 #include<windows.h>
-using namespace std;
-void Print(int x){
-    system("cls");
-    for(int i=0;i<x;++i)
-        printf(" ");
-    printf("x");
+int get_width(){
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int Width;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        Width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        return Width;
+    } else {
+        printf("Failed to get console info.\n");
+		exit(0);
+    }
+    return 0;
+}
+void Setpos(int x,int y){
+	COORD pos;
+	pos.X=x;
+	pos.Y=y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+}
+void Hide() {
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);  
+	CONSOLE_CURSOR_INFO CursorInfo;  
+	GetConsoleCursorInfo(handle, &CursorInfo); 
+	CursorInfo.bVisible = 0;  
+	SetConsoleCursorInfo(handle, &CursorInfo);
+}
+void output(int x,int y,const char *s){
+	Setpos(x,y);
+	printf("%s",s);
 }
 int main(){
-    int x=0,dx[]={1,-1},op=1;
+    Hide();
+    int x=0,px=0,dx=1,w=get_width();
     while(1){
-        Print(x);
-        x=x+dx[op];
-        op^=(x==0||x==80);
+        output(x,0,"x");
+        output(px,0," ");
+        px=x;
+        x+=dx;
+        if(x==0||x==w) dx*=-1;
         Sleep(50);
     }
     return 0;
